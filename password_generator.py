8ae4c9f923984c9ffedb379348a1a314a96c25dd
nasar <muhammadnasar012@gmail.com>
2023-12-14 12:46:30 +0500

    Initial commit

--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import string
 import random
 
-app = Flask(__name__)
+password_generator = Flask(__name__)
 
 def generate_password(length=12, use_symbols=True):
     characters = string.ascii_letters + string.digits
@@ -12,14 +12,14 @@ def generate_password(length=12, use_symbols=True):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-@app.route('/', methods=['GET', 'POST'])
+@password_generator.route('/', methods=['GET', 'POST'])
 def index():
     generated_password = ''
     if request.method == 'POST':
         password_length = int(request.form['password_length'])
         include_symbols = 'include_symbols' in request.form
         generated_password = generate_password(password_length, include_symbols)
-    return render_template('index.html', generated_password=generated_password)
+    return render_template('password_generator.html', generated_password=generated_password)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    password_generator.run(debug=True)
